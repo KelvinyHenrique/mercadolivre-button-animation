4c96632b26d512323e311462fad2de4d15a13ff6
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2021-08-22 15:14:07 -0300

    First Commit

---
+++ src/components/Button.vue
@@ -0,0 +1,121 @@
+<template>
+  <button class="k-btn" :style="styles">
+    <span
+      class="k-progress__bar"
+      :style="loadStyles"
+      :class="loading ? 'loading' : null"
+    />
+    <span class="k-button__text"> {{ title }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  computed: {
+    loadStyles() {
+      return {
+        transitionDuration: this.loadingTime,
+        background: this.bgProgress,
+        borderRadius: this.borderRadius,
+      };
+    },
+    styles() {
+      return {
+        "--background-hover": this.hoverBackground,
+        "--background": this.background,
+        "--text-color": this.textColor,
+        "--text-hover-color": this.textHoverColor,
+        fontSize: this.textSize,
+        borderRadius: this.borderRadius,
+      };
+    },
+  },
+  props: {
+    title: {
+      type: String,
+      default: "Button",
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    loadingTime: {
+      type: Number,
+      default: 1,
+    },
+    bgProgress: {
+      type: String,
+      default: "#5c95ff",
+    },
+    hoverBackground: {
+      type: String,
+      default: "#3483facc",
+    },
+    textColor: {
+      type: String,
+      default: "#FFF",
+    },
+    textHoverColor: {
+      type: String,
+      default: "#FFF",
+    },
+    background: {
+      type: String,
+      default: "#3483fa",
+    },
+    fontSize: {
+      type: String,
+      default: "16px",
+    },
+    borderRadius: {
+      type: String,
+      default: "4px",
+    },
+  },
+})
+export default class HelloWorld extends Vue {
+  title!: string;
+  loading!: boolean;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.k-btn {
+  all: unset;
+  position: relative;
+  background: var(--background);
+  color: var(--text-color);
+  font-weight: 600;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  border-radius: 4px;
+  width: 250px;
+  cursor: pointer;
+}
+.k-button__text {
+  position: relative;
+  padding-left: 32px;
+  padding-right: 32px;
+}
+.k-progress__bar {
+  top: 0;
+  left: 0;
+  transition-property: all;
+  transition-duration: 1s;
+  transition-timing-function: ease;
+  transition-delay: 0s;
+  position: absolute;
+  width: 0;
+  height: 100%;
+}
+.loading {
+  width: 100%;
+}
+.k-btn:hover {
+  background-color: var(--background-hover);
+  color: var(--text-hover-color);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Button
+      :loading="isLoading"
+      @click="isLoading = !isLoading"
+      loading-time="2s"
+      title="Confirmar compra"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Button from "@/components/Button.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    Button,
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
   },
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  background-color: #121212;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
